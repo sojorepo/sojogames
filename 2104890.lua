dfaaeadd01ffed9662636124edafaa58a5776201
sojorepo <thepoorgamer2424@gmail.com>
2025-06-16 04:59:29 +0800

    Add files via upload

---
+++ 2104890.lua
@@ -0,0 +1,23 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+-- Steam App 2104890 Manifest
+-- Name: RoadCraft
+-- Generated: 2025-05-22 16:41:33
+-- Total Depots: 2
+-- Total DLCs: 4
+
+-- MAIN APPLICATION
+addappid(2104890) -- RoadCraft
+
+-- MAIN APP DEPOTS
+addappid(2104891, 1, "7aae8e21e13a76d7d261e23592399805131dbb2dc4678b5c40b335e6f131b3a7") -- Main Game Content (Windows Content)
+setManifestid(2104891, "6682696923648315772", 0)
+addappid(3510890, 1, "fed6ac5a7ee3d94e10e924fe19f30acfcbcfdb56a5835b080efede124d44b8a0") -- Game Content (Depot 3510890)
+setManifestid(3510890, "8716409139232890468", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2806770) -- RoadCraft  Aramatsu Bowhead 30T
+addappid(3470710) -- RoadCraft  Invictus Type A Scout
+addappid(3470730) -- RoadCraft - Rebuild Expansion

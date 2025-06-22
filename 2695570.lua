d45c958536478fdc055a2e6c313e87aea8ac596f
sojorepo <thepoorgamer2424@gmail.com>
2025-06-23 04:01:31 +0800

    Add files via upload

---
+++ 2695570.lua
@@ -0,0 +1,11 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2695570)
+addappid(2695571, 1, "45055cb777941c2a8648fa0823935affbd3d7e4eb93d62ce2e4289608414f55e")
+setManifestid(2695571, "1341008708681182767", 0)
+addappid(2695572, 1, "477df0bff7208d22036f448857f95366b94b879a57c6c44ee4ae7e112a783b5c")
+setManifestid(2695572, "8122264098667267962", 0)
+addappid(2695573, 1, "9e5c97734a758d85c533da72dd16b04f636403392150b6a2cdd0049396c8253f")
+setManifestid(2695573, "3403287061928872296", 0)

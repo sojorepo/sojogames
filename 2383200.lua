00b413540603646f156006a191d08fc41241dd62
sojorepo <thepoorgamer2424@gmail.com>
2025-07-12 16:09:03 +0800

    Add files via upload

---
+++ 2383200.lua
@@ -0,0 +1,13 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2383200 Manifest
+-- Name: PATAPON 1+2 REPLAY
+-- Generated: 2025-07-11 12:32:17
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2383200) -- PATAPON 1+2 REPLAY
+
+-- MAIN APP DEPOTS
+addappid(2383201, 1, "e6ab327b2ce7b1d4a38492ab781adbe69841ef9b0a99d0d939c6c9d65eed4950") -- Depot 2383201
+setManifestid(2383201, "4584414798360174724", 0)

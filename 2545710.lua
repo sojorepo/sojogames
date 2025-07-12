d54b3950e0e0deed2e92127b28f5d0075b549e52
sojorepo <thepoorgamer2424@gmail.com>
2025-07-12 21:25:43 +0800

    Add files via upload

---
+++ 2545710.lua
@@ -0,0 +1,31 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2545710 Manifest
+-- Name: Tony Hawk's™ Pro Skater™ 3 + 4
+-- Generated: 2025-07-11 20:21:38
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2545710) -- Tony Hawk's™ Pro Skater™ 3 + 4
+
+-- MAIN APP DEPOTS
+addappid(2545711, 1, "cc91db4546139b89ff7216df6c489b4c157da8515a88f2491921ae845b9dab5a") -- Depot 2545711
+setManifestid(2545711, "6481483883981221361", 0)
+addappid(2545712, 1, "8027343d1efbd17177d104a56462394d6507a9d68f348e96e365b9d9db954804") -- Depot 2545712
+setManifestid(2545712, "6429272888018540261", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3315210) -- Tony Hawks Pro Skater 3  4 - Digital Deluxe Edition Content
+addappid(3529150) -- Tony Hawks Pro Skater 3  4 - Wireframe Tony Shader
+addappid(3529160) -- Tony Hawks Pro Skater 3  4 - Content Pack
